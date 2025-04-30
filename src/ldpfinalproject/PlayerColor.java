bd1a7aa42e987e76689adf4a7dc31ed12f5a64a8
patricia <patri@DESKTOP-DNKF1V6.lan>
2025-04-30 20:10:04 +0100

    Creation of a class to manage colors

---
+++ src/ldpfinalproject/PlayerColor.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package ldpfinalproject;
+
+/**
+ *
+ * @author patri
+ */
+// PlayerColor.java (enum de cores do jogador)
+public enum PlayerColor {
+    RED, GREEN, BLUE, YELLOW;
+}
+
